7c898c481cdf1ddc11872e0fac3834f05e3ae800
Pandalf974 <klael38@proton.me>
2025-04-27 09:02:53 +0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import EbookForm from './EbookForm';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-8">
+      <EbookForm />
+    </div>
+  );
+}
+
+export default App;
